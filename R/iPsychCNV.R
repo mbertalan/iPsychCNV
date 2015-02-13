08ebf358fdd62d861e86a5f598f6cc2835a304a6
Marcelo Bertalan <mbertalan@gmail.com>
2015-02-13 17:45:07 +0100

    Update iPsychCNV.R

--- R/iPsychCNV.R
+++ R/iPsychCNV.R
@@ -6,7 +6,7 @@
 ##' @author Marcelo Bertalan
 ##' @export
 
-iPsychCNV <- function(PathRawData = "/media/NeoScreen/NeSc_home/ILMN/iPSYCH/", MINNumSNPs=100, Cores=10, NumFiles="All", Pattern="22q11_*", MinLength=100000, SelectedFiles=NA, NormQspline=FALSE, DFspline=NA, Skip=10) # Files2 OutputPath
+iPsychCNV <- function(PathRawData = "/media/NeoScreen/NeSc_home/ILMN/iPSYCH/", MINNumSNPs=100, Cores=10, NumFiles="All", Pattern="22q11_*", MinLength=100000, SelectedFiles=NA, NormQspline=FALSE, DFspline=NA, Skip=10, LCR=TRUE) # Files2 OutputPath
 {	
 	if(file.exists("Progress.txt")){ file.remove("Progress.txt") }
 
@@ -39,7 +39,7 @@ iPsychCNV <- function(PathRawData = "/media/NeoScreen/NeSc_home/ILMN/iPSYCH/", M
 		ID <- tail(unlist(strsplit(X, "/")),n=1)
 	
 		# Read CNV file		
-		CNV <- ReadCNV(RawFile, skip=Skip)
+		CNV <- ReadCNV(RawFile, skip=Skip, LCR=TRUE)
 		
 		# Normalize data
 		CNV <- NormalizeData(CNV, ExpectedMean=0, DF=DFspline, NormQspline)
