14f93d7e38f4864f9605be5d6d51a8f3c1634150
Marcelo Bertalan <mbertalan@gmail.com>
2015-02-16 12:50:27 +0100

    Update PlotAllInOnePlotggplot.R

--- R/PlotAllInOnePlotggplot.R
+++ R/PlotAllInOnePlotggplot.R
@@ -1,6 +1,5 @@
-##' iPsychCNV: Find Copy Number Variation (CNV) from SNP genotyping arrays. 
+##' PlotAllinOnePlotggplot: Plot all CNVs in one plot. 
 ##'
-##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
 ##' @title PlotAllinOnePlotggplot
 ##' @return return data in data frame
 ##' @author Marcelo Bertalan
