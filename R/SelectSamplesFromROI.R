f24246935bb67e097aa90042abb8b416ee420593
Marcelo Bertalan <mbertalan@gmail.com>
2015-02-23 11:55:46 +0100

    Update and rename R to R/SelectSamplesFromROI.R

---
+++ R/SelectSamplesFromROI.R
@@ -0,0 +1,49 @@
+SelectSamplesFromROI <- function(DF, roi, OverlapMin, OverlapMax)
+{
+	tmp2 <- apply(roi, 1, function(X)
+	{
+		cat(X, "\n")
+		LengthRoi <- as.numeric(X["Length"])
+		StartRoi <- as.numeric(X["Start"])
+		StopRoi <- as.numeric(X["Stop"])
+		ChrRoi <- as.character(X["Chr"])
+		tmp <- subset(DF, Chr %in% ChrRoi & Start < StopRoi & Stop > StartRoi)  # & ((Length/LengthRoi) >  Overlap)
+		if(nrow(tmp) > 0)
+		{
+			#Overlap
+			Overlap <- apply(tmp, 1, function(Y)
+			{
+				Length <- as.numeric(Y["Length"])
+				Start <- as.numeric(Y["Start"])
+				Stop <- as.numeric(Y["Stop"])
+				#cat(Start, Stop, StartRoi, StopRoi, "\n")
+				if(Start >= StartRoi & Stop >= StopRoi) 
+				{				     
+					   Overlaplength <- (Length/(StopRoi - Start))
+				}
+				if(Start <= StartRoi & Stop <= StopRoi)
+				{					
+					Overlaplength <- (Length/(StartRoi - Stop))
+				}
+				if(Start >= StartRoi & Stop <= StopRoi)
+				{
+					Overlaplength <- (Length/Length)
+				}
+				if(Start <= StartRoi & Stop >= StopRoi)
+				{
+					Overlaplength <- (Length/LengthRoi)
+				}
+				return(Overlaplength)
+			})
+			tmp$Overlap <- Overlap
+			#Ratio <- tmp$Length/LengthRoi
+			#tmp$Overlap <- Ratio
+			tmp <- subset(tmp, Overlap > OverlapMin & Overlap < OverlapMax)
+			#save(tmp, file="tmp.RData")
+			#Samples <- unique(tmp$ID)
+			return(tmp)
+		}
+	})
+	tmp3 <- MatrixOrList2df(tmp2)
+	return(tmp3)
+}
