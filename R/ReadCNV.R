161e2abc55fd92780b8d09231b658247a06e846f
Marcelo Bertalan <mbertalan@gmail.com>
2015-03-09 19:09:02 +0100

    Update ReadCNV.R

--- R/ReadCNV.R
+++ R/ReadCNV.R
@@ -12,6 +12,7 @@ ReadCNV <- function(RawFile="Test.txt", skip=0, LCR=FALSE)
 	CNV <- fread(RawFile, head=T, sep="\t", skip=skip)
 	CNV <- as.data.frame(CNV)
 	colnames(CNV) <- gsub(" ", ".", colnames(CNV))
+	colnames(CNV)[colnames(CNV) %in% "Name"] <- "SNP.Name"
 	CNV <- CNV[,c("SNP.Name","Chr", "Position", "Log.R.Ratio", "B.Allele.Freq")] # SNP.Name
 	CNV <- subset(CNV, !Chr %in% c("MT", "X", "Y", "XY", "0"))
 	CNV <- subset(CNV, !is.na(CNV$B.Allele.Freq))
