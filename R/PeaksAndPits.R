263e664da1cb1c4c01d06fbcc443746914815f0c
Marcelo Bertalan <mbertalan@gmail.com>
2015-03-12 13:54:43 +0100

    Update PeaksAndPits.R

--- R/PeaksAndPits.R
+++ R/PeaksAndPits.R
@@ -21,31 +21,35 @@ PeaksAndPits <- function(DF, PlotName)
 		tmp <- smooth.spline(1:nrow(subCNV), subCNV$Log.R.Ratio)
 		tp <- turnpoints(tmp$y)
 		
-		NewName <- paste(PlotName, "_chr", Z, ".png", sep="", collapse="")
 			
-		
-		PointTest <- turning.point.test(subCNV$Log.R.Ratio)$p.value
-		Peaks <- c(1,which(tp$peaks))
-		Pits <- c(1,which(tp$pits))
+		if(!is.na(tp$tppos))
+		{
+			PointTest <- turning.point.test(subCNV$Log.R.Ratio)$p.value
+			Peaks <- c(1,which(tp$peaks))
+			Pits <- c(1,which(tp$pits))
 
-		WaveLenPeaks <- sapply(2:length(Peaks), function(X){ Peaks[X] - Peaks[X-1] })
-		WaveLenPits <- sapply(2:length(Pits), function(X){ Pits[X] - Pits[X-1] })
+			WaveLenPeaks <- sapply(2:length(Peaks), function(X){ Peaks[X] - Peaks[X-1] })
+			WaveLenPits <- sapply(2:length(Pits), function(X){ Pits[X] - Pits[X-1] })
 
-		
-		WaveLength <- rep(0, length(tp$peaks))
-		WaveLength[tp$peaks] <- WaveLenPeaks
-		WaveLength[tp$pits] <- WaveLenPits
-		Wave <- WaveLength[WaveLength > 0]
+			WaveLength <- rep(0, length(tp$peaks))
+			WaveLength[tp$peaks] <- WaveLenPeaks
+			WaveLength[tp$pits] <- WaveLenPits
+			Wave <- WaveLength[WaveLength > 0]
 
-		Peaks <- c(1,which(tp$peaks))
-		Pits <- c(1,which(tp$pits))
-		Indx <- sort(c(Peaks,Pits))[-1]
-		Points <- tp$points[Indx]
-		Points2 <- Points + abs(min(Points))
-		Amplitude <- sapply(1:(length(Points2)-1), function(X){ Points2[X] - Points2[X+1] })
-
+			Indx <- sort(c(Peaks,Pits))[-1]
+			Points <- tp$points[Indx]
+			Points2 <- Points + abs(min(Points))
+			Amplitude <- sapply(1:(length(Points2)-1), function(X){ Points2[X] - Points2[X+1] })
+		}
+		else
+		{
+			tp$proba <- 0
+			tp$tppos <- 0
+			Wave <- 0
+			Amplitude <- 0
+		}
 		df <- data.frame(Position=tp$tppos, proba=tp$proba, WaveLength=Wave, Amplitude=Amplitude, stringsAsFactors=F)
-
+		
 		Log.R.Ratio.Median <- median(subCNV$Log.R.Ratio)
 		Log.R.Ratio.Mean <- mean(subCNV$Log.R.Ratio)
 		Log.R.Ratio.SD <- sd(subCNV$Log.R.Ratio)
@@ -63,8 +67,6 @@ PeaksAndPits <- function(DF, PlotName)
 		BAF_Table <- (BAF_Table/sum(BAF_Table))*100
 		BAF_Drift_Median <- median(BAF_Res$Drift)
 		BAF_Drift_SD <- sd(BAF_Res$Drift)
-		
-		
 
 		df2 <- data.frame(Log.R.Ratio.Median=Log.R.Ratio.Median, Log.R.Ratio.Mean=Log.R.Ratio.Mean, Log.R.Ratio.SD=Log.R.Ratio.SD, B.Allele.Freq.Median=B.Allele.Freq.Median,B.Allele.Freq.Mean=B.Allele.Freq.Mean, B.Allele.Freq.SD =B.Allele.Freq.SD , Wave.Length.Mean=Wave.Length.Mean, Wave.Length.SD=Wave.Length.SD, Amplitude.Mean =Amplitude.Mean , Amplitude.SD=Amplitude.SD, PointTest=PointTest, BAF_Drift_Median=BAF_Drift_Median, BAF_Drift_SD=BAF_Drift_SD, AAAA=BAF_Table["AAAA"], AAAB=BAF_Table["AAAB"], AAB=BAF_Table["AAB"],AB=BAF_Table["AB"], ABB=BAF_Table["ABB"], ABBB=BAF_Table["ABBB"], BBBB=BAF_Table["BBBB"], Chr=as.numeric(Z), ID=PlotName,stringsAsFactors=F)
 
