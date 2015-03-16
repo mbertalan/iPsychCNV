0c6b8b5a4297ca67accb8d2cd81e4c3ddde61860
Marcelo Bertalan <mbertalan@gmail.com>
2015-03-16 15:33:04 +0100

    Update GetMockValues.R

--- R/GetMockValues.R
+++ R/GetMockValues.R
@@ -1,4 +1,4 @@
-GetMockValues <- function(Type="Blood", BAF_LOH=FALSE)
+GetMockValues <- function(Type="Blood")
 {
 	# BAF Basic
 	BAFs <- seq(from=0, to=1, by=0.01) # 101
@@ -16,48 +16,17 @@ GetMockValues <- function(Type="Blood", BAF_LOH=FALSE)
 	BadSNPIntensityProb <- seq(from=0.53, to=0.11, by=-0.01)
 
 	# From BAF basic to Normal.
-	if(BAF_LOH)
-	{
-		BAF_Basic[10:90] <- seq(from=0.18, to=0.22, by=0.0005)
-	
-		# BAF normal prob	
-		BAF_Normal <- BAF_Basic
-		BAF_Normal[c(1:2)] <- BAF_Normal[c(1:2)] + 0.2
-		BAF_Normal[c(3:4)] <- BAF_Normal[c(3:4)] + 0.1
-		BAF_Normal[c(8:9)] <- BAF_Normal[c(8:9)] - 0.02
-
-		BAF_Normal[c(80:85)] <- BAF_Normal[c(80:85)] + 0.01
-		BAF_Normal[c(98:99)] <- BAF_Normal[c(98:99)] + 0.20
-		BAF_Normal[c(100:101)] <- BAF_Normal[c(100:101)] + 0.5	
-	}
-	else
-	{		
-		if(Type %in% "Blood")
-		{
-			BAF_Normal <- BAF_Basic
-			BAF_Normal[c(1:2)] <- BAF_Normal[c(1:2)] + 0.52
-			BAF_Normal[c(3:4)] <- BAF_Normal[c(3:4)] + 0.32
-			BAF_Normal[c(100:101)] <- BAF_Normal[c(100:101)] + 0.6
-			BAF_Normal[c(98:99)] <- BAF_Normal[c(98:99)] + 0.35
-			BAF_Normal[c(45:55)] <- BAF_Normal[c(45:55)] + 0.05
-			BAF_Normal[c(47:53)] <- BAF_Normal[c(47:53)] + 0.05
-			BAF_Normal[c(50:51)] <- BAF_Normal[c(50:51)] + 0.1
-		}
-		else # PKU
-		{
-			BAF_Normal <- BAF_Basic + 0.03
-			BAF_Normal[c(1:2)] <- BAF_Normal[c(1:2)] + 0.5
-			BAF_Normal[c(3:4)] <- BAF_Normal[c(3:4)] + 0.3
-			BAF_Normal[c(100:101)] <- BAF_Normal[c(100:101)] + 0.95
-			BAF_Normal[c(98:99)] <- BAF_Normal[c(98:99)] + 0.75
-			BAF_Normal[c(45:55)] <- BAF_Normal[c(45:55)] + 0.03
-			BAF_Normal[c(47:53)] <- BAF_Normal[c(47:53)] + 0.05
-			BAF_Normal[c(50:51)] <- BAF_Normal[c(50:51)] + 0.05
-		}
-	}
-
 	if(Type %in% "Blood")
 	{
+		BAF_Normal <- BAF_Basic
+		BAF_Normal[c(1:2)] <- BAF_Normal[c(1:2)] + 0.52
+		BAF_Normal[c(3:4)] <- BAF_Normal[c(3:4)] + 0.32
+		BAF_Normal[c(100:101)] <- BAF_Normal[c(100:101)] + 0.6
+		BAF_Normal[c(98:99)] <- BAF_Normal[c(98:99)] + 0.35
+		BAF_Normal[c(45:55)] <- BAF_Normal[c(45:55)] + 0.05
+		BAF_Normal[c(47:53)] <- BAF_Normal[c(47:53)] + 0.05
+		BAF_Normal[c(50:51)] <- BAF_Normal[c(50:51)] + 0.1
+		
 		# CNV Info
 		Del <- seq(from=-0.3, to=-0.5, by=-0.05)
 		Dup <- seq(from=0.3, to=0.5, by=0.05)
@@ -83,8 +52,17 @@ GetMockValues <- function(Type="Blood", BAF_LOH=FALSE)
 		BAF_Dup[25:35] <- BAF_Dup[25:35] + 0.2 # 0.25 0.30 0.35, 
 		BAF_Dup[70:80] <- BAF_Dup[70:80] + 0.2 # 0.7 0.75 0.8	
 	}
-	else
+	else # PKU
 	{
+		BAF_Normal <- BAF_Basic + 0.03
+		BAF_Normal[c(1:2)] <- BAF_Normal[c(1:2)] + 0.5
+		BAF_Normal[c(3:4)] <- BAF_Normal[c(3:4)] + 0.3
+		BAF_Normal[c(100:101)] <- BAF_Normal[c(100:101)] + 0.95
+		BAF_Normal[c(98:99)] <- BAF_Normal[c(98:99)] + 0.75
+		BAF_Normal[c(45:55)] <- BAF_Normal[c(45:55)] + 0.03
+		BAF_Normal[c(47:53)] <- BAF_Normal[c(47:53)] + 0.05
+		BAF_Normal[c(50:51)] <- BAF_Normal[c(50:51)] + 0.05
+		
 		# CNV Info
 		Del <- seq(from=-0.15, to=-0.5, by=-0.05)
 		Dup <- seq(from=0.15, to=0.5, by=0.05)
@@ -110,7 +88,6 @@ GetMockValues <- function(Type="Blood", BAF_LOH=FALSE)
 		BAF_Dup[70:80] <- BAF_Dup[70:80] + 0.15 # 0.7 0.75 0.8
 		
 	}
-	
-	tmp <- list(Del=Del, Dup=Dup, ChrMean=ChrMean, ChrSD=ChrSD, ChrSDProb=ChrSDProb, TelomereNoiseSize=TelomereNoiseSize, TelomereNoiseEffect=TelomereNoiseEffect, BAF_Normal=BAF_Normal, BAF_Del=BAF_Del, BAF_Dup=BAF_Dup, BadSNPs=BadSNPs, BadSNPIntensity=BadSNPIntensity, BadSNPIntensityProb=BadSNPIntensityProb, BAFs=BAFs, ChrMeanProb=ChrMeanProb)
+	tmp <- list(Del=Del, Dup=Dup, ChrMean=ChrMean, ChrSD=ChrSD, ChrSDProb=ChrSDProb, TelomereNoiseSize=TelomereNoiseSize, TelomereNoiseEffect=TelomereNoiseEffect, BAF_Normal=BAF_Normal, BAF_Del=BAF_Del, BAF_Dup=BAF_Dup, BadSNPs=BadSNPs, BadSNPIntensity=BadSNPIntensity, BadSNPIntensityProb=BadSNPIntensityProb, BAFs=BAFs, ChrMeanProb=ChrMeanPro
 	return(tmp)
 }
