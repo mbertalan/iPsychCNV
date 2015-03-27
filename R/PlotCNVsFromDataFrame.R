034e77c38933d26047dcec95a8e6b69d060dfe73
Marcelo Bertalan <mbertalan@gmail.com>
2015-03-27 09:42:11 +0100

    Update PlotCNVsFromDataFrame.R

--- R/PlotCNVsFromDataFrame.R
+++ R/PlotCNVsFromDataFrame.R
@@ -1,13 +1,19 @@
-PlotCNVsFromDataFrame <- function(DF, Cores) # Path from cluster  
+PlotCNVsFromDataFrame <- function(DF, Cores, PathRawData,Pattern) # Path from cluster  
 {
 	library(mclust)
 	library(parallel)
 
+	Files <- list.files(path=PathRawData, pattern=Pattern, full.names=TRUE, recursive=TRUE) # Checking all files at the folder
+	Files <- sapply(DF$ID, function(X){ Files[grep(X, Files)] }) # Selecting only those that are in the data.frame
+	
 	DF$UniqueID <- 1:nrow(DF)
 	matrixList <- lapply(1:NROW(as.matrix(DF)), function(i) DF[i,,drop=FALSE]) 
+	
+	tmp <- mclapply(Files, mc.cores=Cores, mc.preschedule = FALSE, function(X) 
+	{
+		RawFile <- X
 	mclapply(matrixList, mc.cores=Cores, function(X) 
 	{
-		
 		NAMES <- names(X)
 		X2 <- unlist(X)
 		names(X2) <- NAMES
@@ -24,30 +30,16 @@ PlotCNVsFromDataFrame <- function(DF, Cores) # Path from cluster
 		CNVstart <- as.numeric(X["Start"])
 		CNVstop <- as.numeric(X["Stop"])
 		Size <- as.numeric(X["Length"])
-		#Source <- X["Source"]
-		CN <- X["Class"]
+		CN <- X["CN"]
 		SDCNV <- round(as.numeric(X["SDCNV"]), digits=2)
 		NumSNP <- as.numeric(X["NumSNPs"])
 
-		#if("PlotStart" %in% names(X))
-		#{
-		#	Start <- as.numeric(X["PlotStart"])
-		#	Stop <- as.numeric(X["PlotStop"])
-		#}
-		#else
-		#{
-			Start <- CNVstart - Size*(3/log10(NumSNP))^3
-			Stop <-  CNVstop + Size*(3/log10(NumSNP))^3
-		#}
+		Start <- CNVstart - Size*(3/log10(NumSNP))^3
+		Stop <-  CNVstop + Size*(3/log10(NumSNP))^3
+
 		Conf <- 100
 		NewName <- paste(UniqueID, CNVstart, CNVstop, chr, ID, sep="_", collapse="")
 		OutPlotfile <- paste(NewName, "plot.png", sep=".", collapse="")
- 		#cat(ID, CN, "\n")
-		# Version 3
-		# Include Overlap cutoff
-		# Relative size to CNV
-		# Start and stop = Region to plot
-		# CNVstart, CNVstop, = CNV region.
 	
 		library(biovizBase)
 		library(GenomeGraphs)
@@ -64,18 +56,15 @@ PlotCNVsFromDataFrame <- function(DF, Cores) # Path from cluster
 		cat(RawFile,"\n")
 		sample<-read.table(RawFile,as.is=T,head=T,sep="\t", skip=10)
 		red<-subset(sample,Chr==chr)
-		save(red, file="red.RData")	
 		red <-subset(red, Position > Start & Position < Stop)	
 		red$Log.R.Ratio[is.na(red$Log.R.Ratio)] <- 0
 
 		red2 <- red[with(red, order(Position)),]
-		save(red2, file="red2.RData")	
 		red2$Log.R.Ratio[is.na(red2$Log.R.Ratio)] <- 0
-		source("~/CNV/Scripts/iPsychCNV/SlidingWindowMean.R")
+		#source("~/CNV/Scripts/iPsychCNV/SlidingWindowMean.R")
 		Mean <- SlideWindowMean(red2$Log.R.Ratio, 35)
 		red2$Mean <- Mean
 		
-
 		# Ideogram
 		data(hg19IdeogramCyto,package="biovizBase")
 		CHR <- paste("chr", chr, collapse="", sep="")
@@ -92,7 +81,7 @@ PlotCNVsFromDataFrame <- function(DF, Cores) # Path from cluster
 		# LogRRatio
 		p2 <- ggplot(red2, aes(Position, y = Log.R.Ratio, col="Log.R.Ratio")) + geom_point(alpha = 0.6, size=1) + geom_line(aes(x=Position, y = Mean, col="Mean"), size = 0.5) + ylim(-1, 1) + theme(legend.title=element_blank()) + scale_color_manual(values = c(Colors[1], "black"))
 
-		Title <- paste("CN: ", CN, ", SDCNV: ", SDCNV, ", NumSNPs: ", NumSNP, ", Sample: ", ID, sep="", collapse="")
+		Title <- paste("CN: ", CN, ", NumSNPs: ", NumSNP, ", Sample: ", ID, sep="", collapse="")
 		Plot <- tracks(p3,p1, p2, main=Title, heights=c(3,5,5))
 		ggsave(OutPlotfile, plot=Plot, height=5, width=10, dpi=100) 
 	})
