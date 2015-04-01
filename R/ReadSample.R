be4372e4f805367ce2668f1f50ef58751892c4b3
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-01 12:45:35 +0200

    Update ReadSample.R

--- R/ReadSample.R
+++ R/ReadSample.R
@@ -6,7 +6,7 @@
 ##' @author Marcelo Bertalan
 ##' @export
 
-ReadSample <- function(RawFile="Test.txt", skip=0, LCR=FALSE)
+ReadSample <- function(RawFile="Test.txt", skip=0, LCR=FALSE, PFB)
 {
 	suppressPackageStartupMessages(library(data.table))
 	CNV <- fread(RawFile, head=T, sep="\t", skip=skip)
@@ -14,6 +14,9 @@ ReadSample <- function(RawFile="Test.txt", skip=0, LCR=FALSE)
 	colnames(CNV) <- gsub(" ", ".", colnames(CNV))
 	colnames(CNV)[colnames(CNV) %in% "Name"] <- "SNP.Name"
 	CNV <- CNV[,c("SNP.Name","Chr", "Position", "Log.R.Ratio", "B.Allele.Freq")] # SNP.Name
+	
+	if(is.null(PFB)){ CNV$PFB <- rep(0.5, nrow(CNV)) }else{ CNV$PFB <- PFB }
+	
 	CNV <- subset(CNV, !Chr %in% c("MT", "X", "Y", "XY", "0"))
 	CNV <- subset(CNV, !is.na(CNV$B.Allele.Freq))
 	CNV <- subset(CNV, !is.na(CNV$Log.R.Ratio))
