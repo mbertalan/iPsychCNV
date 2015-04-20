f9243699b66efa8fa5eef31529c7ebc73c46e511
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-20 15:02:55 +0200

    Update FilterFromCNVs.R

--- R/FilterFromCNVs.R
+++ R/FilterFromCNVs.R
@@ -19,7 +19,7 @@ FilterFromCNVs <- function(CNVs, PathRawData, MinNumSNPs=10, Source="iPsychCNV",
 			StartIndx <- which(subCNV$Position == StartM)[1] 
 			StopIndx <- which(subCNV$Position == StopM)[1]
 			Vector <- c(StartIndx, StopIndx, X)
-			cat(StartM, StopM, StartIndx, StopIndx, "\n")
+			cat(ChrM, StartM, StopM, StartIndx, StopIndx, "\n")
 			names(Vector)[1:2] <- c("StartIndx", "StopIndx")
 			return(Vector)
 		})
