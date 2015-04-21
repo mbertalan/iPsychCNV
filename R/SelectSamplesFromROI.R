db46b5a63f0134c742fe67b549ed0278b1ab3948
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-21 13:21:02 +0200

    Update SelectSamplesFromROI.R

--- R/SelectSamplesFromROI.R
+++ R/SelectSamplesFromROI.R
@@ -7,6 +7,7 @@ SelectSamplesFromROI <- function(DF, roi, OverlapMin, OverlapMax)
 		StartRoi <- as.numeric(X["Start"])
 		StopRoi <- as.numeric(X["Stop"])
 		ChrRoi <- as.character(X["Chr"])
+		Locus <- as.character(X["locus"])
 		tmp <- subset(DF, Chr %in% ChrRoi & Start < StopRoi & Stop > StartRoi)  # & ((Length/LengthRoi) >  Overlap)
 		if(nrow(tmp) > 0)
 		{
@@ -20,23 +21,30 @@ SelectSamplesFromROI <- function(DF, roi, OverlapMin, OverlapMax)
 				if(Start >= StartRoi & Stop >= StopRoi) # |-->--|---<
 				{				     
 					   Overlaplength <- ((StopRoi - Start)/LengthRoi)
+					   OverlapAll <- Length/LengthRoi
 				}
 				if(Start <= StartRoi & Stop <= StopRoi) # >---|----<----|
 				{					
 					Overlaplength <- ((Stop - StartRoi)/LengthRoi)
+					OverlapAll <- Length/LengthRoi
 				}
 				if(Start >= StartRoi & Stop <= StopRoi) # |--->-----<---|
 				{
 					Overlaplength <- (Length/LengthRoi)
+					OverlapAll <- Length/LengthRoi
 				}
 				if(Start <= StartRoi & Stop >= StopRoi) # >---|-------|---<
 				{
 					Overlaplength <- (Length/LengthRoi)
+					OverlapAll <- Length/LengthRoi
 				}
-				return(Overlaplength)
+				df <- data.frame(Overlaplength=Overlaplength, OverlapAll=OverlapAll)
+				return(df)
 			})
-			tmp$Overlap <- Overlap
-			tmp <- subset(tmp, Overlap > OverlapMin & Overlap < OverlapMax)
+			Overlap <- MatrixOrList2df(Overlap)
+			tmp2 <- cbind(tmp, Overlap)
+			tmp2$Locus <- rep(Locus, nrow(tmp2))
+			tmp <- subset(tmp2, OverlapAll > OverlapMin & OverlapAll < OverlapMax)
 			return(tmp)
 		}
 	})
