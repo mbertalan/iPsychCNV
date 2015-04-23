08807b021bfdf549755a41b69e38787799dc20ca
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-23 14:26:13 +0200

    Update DefiningLogRRatio.R

--- R/DefiningLogRRatio.R
+++ R/DefiningLogRRatio.R
@@ -8,40 +8,38 @@
 
 DefiningLogRRatio <- function(res2)
 {
-	#cat("I am in !\n")
-	#cat(res2$SDCNV, res2$CNVmean, "\n")
-	LogRRatio <- "Undefined"
+	LogRRatio <- 2
 	if(res2$SDCNV < 0.7)
 	{
 		if(res2$CNVmeanOrig < -0.1) # 1 or 0
 		{
-			LogRRatio <- "1"
+			LogRRatio <- 1
 			if(res2$CNVmean < res2$HighMean &  res2$CNVmean < res2$LowMean)
 			{
-				LogRRatio <- "1"
+				LogRRatio <- 1
 				if(res2$DiffHigh > 0.7 || res2$DiffLow > 0.7)
 				{
-					if(res2$CNVmean < -2){ LogRRatio <- "0" }else{ LogRRatio <- "1" }
+					if(res2$CNVmean < -2){ LogRRatio <- 0 }else{ LogRRatio <- 1 }
 				}
 				else
 				{
-					LogRRatio <- "1"
+					LogRRatio <- 1
 				}
 			}
 		}
 		else if(res2$CNVmeanOrig > 0.1)
 		{
-			LogRRatio <- "3"
+			LogRRatio <- 3
 			if(res2$CNVmean > res2$HighMean &  res2$CNVmean > res2$LowMean)
 			{
-				LogRRatio <- "3"
+				LogRRatio <- 3
 				if(res2$DiffHigh > 0.8 || res2$DiffLow > 0.8 & res2$CNVmean > 2)
 				{
-					LogRRatio <- "4"
+					LogRRatio <- 4
 				}
 				else
 				{
-					LogRRatio <- "3"
+					LogRRatio <- 3
 				}
 			}
 		}	
