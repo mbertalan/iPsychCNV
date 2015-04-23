ffc3636ed3a42592d2b7a75e54cd8cac6d1a9da4
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-23 14:25:05 +0200

    Update EvaluateMyBAF.R

--- R/EvaluateMyBAF.R
+++ R/EvaluateMyBAF.R
@@ -15,35 +15,35 @@ EvaluateMyBAF <- function(res, res2)
 	{
 		if(res$AAAA > 8  & res$AB < 5 & res$BBBB > 8 & res$ABB < 5 & res$AAB < 5)  # Del
 		{
-			BAlleleFreq <- "1"
+			BAlleleFreq <- 1
 		}
 		else if(res$AAAA > 5 & res$BBBB > 5 & res$AB > 8 & res$AAB > 8 & res$ABB > 8 & res$AAAB > 8 & res$ABBB > 8) # Double del
 		{
-			BAlleleFreq <- "0"
+			BAlleleFreq <- 0
 		}
 		else
 		{
-			BAlleleFreq <- "Undefined"
+			BAlleleFreq <- 2
 		}
 	}
 	else if(res2$CNVmeanOrig > 0.1)
 	{
 		if(res$AAB > 3 & res$AB < 6 & res$ABB > 3) # Dup
 		{
-			BAlleleFreq <- "3"
+			BAlleleFreq <- 3
 		}
 		else if(res$AAAB < 6 & res$ABBB < 6 & res$AB > 4 & res$AAAA > 7 & res$BBBB > 7 & res$AAB > 7 & res$ABB > 7) # Double Dup AB = AABB
 		{
-			BAlleleFreq <- "4"
+			BAlleleFreq <- 4
 		}
 		else
 		{
-			BAlleleFreq <- "Undefined"
+			BAlleleFreq <- 2
 		}
 	}
 	else
 	{
-		BAlleleFreq <- "Undefined"		
+		BAlleleFreq <- 2		
 	}
 	return(BAlleleFreq)
 }
