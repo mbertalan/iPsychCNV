b9d11a38b3ff6f75790b3d45523f2195949f15b4
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-24 11:50:04 +0200

    Update GetGenotypeInfo.R

--- R/GetGenotypeInfo.R
+++ R/GetGenotypeInfo.R
@@ -1,7 +1,11 @@
 GetGenotypeInfo <- function(tmpRaw)
 {
 	Alleles <- c(tmpRaw$Allele1, tmpRaw$Allele2)
-	AllelesPer <- (table(Alleles)/length(Alleles))*100
+	AllelesPer <- c(0,0,0,0,0,0,0)
+	names(AllelesPer) <- c("-","A","C","D","G","I","T")
+	AllelesPer2 <- (table(Alleles)/length(Alleles))*100
+	AllelesPer[names(AllelesPer2)] <- AllelesPer2
+	
 	GC <- AllelesPer["G"] +  AllelesPer["C"]
 	AT <- AllelesPer["A"] +  AllelesPer["T"]
 	Hetero <- apply(tmpRaw[, c("Allele1", "Allele2")], 1, function(X)
@@ -15,7 +19,10 @@ GetGenotypeInfo <- function(tmpRaw)
 			return("heterozygous")
 		}
 	})
-	HeteroPer <- (table(Hetero)/length(Hetero))*100
+	HeteroPer <- c(0,0)
+	names(HeteroPer) <- c("homozygous", "heterozygous")
+	HeteroPer2 <- (table(Hetero)/length(Hetero))*100
+	HeteroPer[names(HeteroPer2)] <- HeteroPer2
 	
 	df <- data.frame(heterozygous=HeteroPer["heterozygous"], homozygous=HeteroPer["homozygous"], GC=GC, AT=AT, "Minus"=AllelesPer["-"], "D"=AllelesPer["D"], "I"=AllelesPer["I"], "A"=AllelesPer["A"], "T"=AllelesPer["T"], "G"=AllelesPer["G"], "C"=AllelesPer["C"], stringsAsFactors=F)
 	return(df)
