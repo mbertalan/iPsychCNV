dbfcbdfcc6ec57fc1a0ee97cb95f785d41eca0d0
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-24 15:53:44 +0200

    Update NormalizeLRR.R

--- R/NormalizeLRR.R
+++ R/NormalizeLRR.R
@@ -6,17 +6,25 @@
 ##' @author Marcelo Bertalan
 ##' @export
 
-NormalizeLRR <- function(X, ExpectedMean=0, DF=40, NormQspline=TRUE) # X is a vector
+NormalizeLRR <- function(X, ExpectedMean=0, DF=NA, NormQspline=FALSE, Quantile=TRUE) # X is a vector
 {
+	library(preprocessCore)
 	#cat("DF spline: ", DF, "\n")
 	if(NormQspline)
 	{
-		if(is.na(DF)){ LRR.spline <- smooth.spline(X)$y }
-		else{  LRR.spline <- smooth.spline(X, df=DF)$y }
+		if(is.na(DF)){ LRR <- smooth.spline(X)$y }
+		else{  LRR <- smooth.spline(X, df=DF)$y }
 	}
+	else if(Quantile)
+	{
+		# Creating perfect data
+		M <- sapply(1:50, function(N){ rnorm(n=length(X), mean=0, sd=0.2) })
+		M2 <- cbind(M, X)
+		M3 <- normalize.quantiles(M2)
+		LRR <- M3[, 51]
 	else
 	{
-		LRR.spline <- X
+		LRR <- X
 	}
-	return(LRR.spline)
+	return(LRR)
 }
