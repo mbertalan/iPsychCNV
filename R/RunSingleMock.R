77d013420e418a8eac8c8bd13e6dee49a82584fa
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-28 18:00:33 +0200

    Update RunSingleMock.R

--- R/RunSingleMock.R
+++ R/RunSingleMock.R
@@ -17,7 +17,10 @@ RunSingleMock <- function(Noise=0, CNVMean=0.4, Name="Test", Method="PennCNV")
 	{
 		PredictedCNV <- iPsychCNV(PathRawData=".", MINNumSNPs=28, Cores=1, Pattern="MockSample_1.tab$", MinLength=10, Skip=0, LCR=FALSE)
 	}
-		
+	else if(Method %in% "Gada")
+	{
+		PredictedCNV <- RunGada(tmp)
+	}	
 
 	Name <- paste(Name, "_Noise", Noise, "_CNVmean", CNVMean, ".png", sep="", collapse="")
 	PlotLRRAndCNVs(PredictedCNV, tmp, CNVMean, Name=Name, Roi=RoiSingleMock)
