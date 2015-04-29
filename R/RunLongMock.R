76e5a2c79303e203067280afa4f875a4548df3ca
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-29 11:47:07 +0200

    Create RunLongMock.R

---
+++ R/RunLongMock.R
@@ -0,0 +1,27 @@
+##' RunLongMock: Run a single mock sample   
+##'
+##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' @title RunSingleMock
+##' @return Classification for LRR.
+##' @author Marcelo Bertalan
+##' @export
+
+RunLongMock <- function(Noise=0, CNVMean=0.4, Name="Test", Method="PennCNV")
+{
+	tmp <- MakeOneMockSample(Noise=Noise, CNVMean=CNVMean)
+	if(Method %in% "PennCNV")
+	{
+		PredictedCNV <- RunPennCNV(PathRawData=".", Pattern="MockSample_1.tab$", Cores=1, Skip=0, Normalization=FALSE, PFB="")
+	}
+	else if(Method %in% "iPsychCNV")
+	{
+		PredictedCNV <- iPsychCNV(PathRawData=".", MINNumSNPs=28, Cores=1, Pattern="MockSample_1.tab$", MinLength=10, Skip=0, LCR=FALSE)
+	}
+	else if(Method %in% "Gada")
+	{
+		PredictedCNV <- RunGada(tmp)
+	}	
+
+	Name <- paste(Name, "_Noise", Noise, "_CNVmean", CNVMean, ".png", sep="", collapse="")
+	PlotLRRAndCNVs(PredictedCNV, tmp, CNVMean, Name=Name, Roi=RoiSingleMock)
+}
