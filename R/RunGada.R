8e2e70c6de4a6d71c0c5aba3ffe54d9044ecef65
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-29 12:02:58 +0200

    Update RunGada.R

--- R/RunGada.R
+++ R/RunGada.R
@@ -1,21 +1,25 @@
-RunGada <- function(MockData=tmp)
+RunGada <- function(MockData=tmp, MinSegLen=50, Tgada=4.5)
 {
 	library(gada)
 	# Creating gen.info for Gada
 	gen.info <- MockData[,1:3]
 	colnames(gen.info) <- c("Name", "chr", "pos")
-	Start <- round(nrow(MockData)/2) # Gada can not run just one chromosome.
-	gen.info$chr[Start:nrow(MockData)] <- "2"
-	dataMock<-setupGADAgeneral(MockData$Log.R.Ratio,gen.info=gen.info)
+	
+	# Gada can not run just one chromosome.
+	NotUsed <- gen.info
+	NotUsed$chr <- 2
+	gen.info <- rbind(gen.info, NotUse)
+	
+	dataMock<-setupGADAgeneral(c(MockData$Log.R.Ratio,MockData$Log.R.Ratio) ,gen.info=gen.info)
 	step1<-SBL(dataMock,  estim.sigma2=TRUE)
-	step2<-BackwardElimination(step1,T=4.5,MinSegLen=3)
+	step2<-BackwardElimination(step1,T=Tgada,MinSegLen=MinSegLen)
 	tmp <- summary(step2)
 	tmp <- tmp[,1:6]
 	colnames(tmp) <- c("Start","Stop", "NumSNPs", "CNVMean", "Chr", "CN")
+	tmp <- subset(tmp, Chr %in% "1")
 	tmp$CN[tmp$CN == 0] <- 2
 	tmp$CN[tmp$CN == 1] <- 3
 	tmp$CN[tmp$CN == -1] <- 1
-	tmp$Chr <- rep("1", nrow(tmp))
 	tmp$ID <- rep("Mock", nrow(tmp)) 
 	PredictedCNV <- tmp
 	return(PredictedCNV)
