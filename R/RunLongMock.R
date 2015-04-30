f45a257cfdbbc9596c9945edcb48efc950ab1055
Marcelo Bertalan <mbertalan@gmail.com>
2015-04-30 16:59:22 +0200

    Update RunLongMock.R

--- R/RunLongMock.R
+++ R/RunLongMock.R
@@ -17,7 +17,7 @@ RunLongMock <- function(Name="Test", Method="PennCNV", HMM="/media/NeoScreen/NeS
 	}
 	else if(Method %in% "iPsychCNV")
 	{
-		PredictedCNV <- iPsychCNV(PathRawData=".", MINNumSNPs=28, Cores=1, Pattern="^LongMockSample.tab$", MinLength=10, Skip=0, LCR=FALSE)
+		PredictedCNV <- iPsychCNV(PathRawData=".", MINNumSNPs=28, Cores=1, Pattern="^LongMockSample.tab$", MinLength=10, Skip=0, LCR=FALSE, Quantile=FALSE, NormMean=FALSE)
 	}
 	else if(Method %in% "Gada")
 	{
