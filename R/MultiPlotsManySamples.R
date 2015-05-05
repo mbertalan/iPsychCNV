584dae5ca592ebafaa7c28954943fad8a1bd941b
Marcelo Bertalan <mbertalan@gmail.com>
2015-05-05 14:43:54 +0200

    Update MultiPlotsManySamples.R

--- R/MultiPlotsManySamples.R
+++ R/MultiPlotsManySamples.R
@@ -11,9 +11,9 @@ MultiPlotsManySamples <- function(df=iPsych_8_Samples_Good, Name="Test.png")
 	Colors <- brewer.pal("Set1", n=9) # + scale_color_brewer(palette="Set1")
 
 	plots <- list()  # new empty list
+	IDs <- sort(unique(df$ID))
 	for (i in 1:length(unique(df$ID))) 
 	{
-		IDs <- unique(df$ID)
 		tmp <- subset(df, ID %in% IDs[i])
 		p1 <- ggplot(Cyto) + geom_segment(aes(x = Start, y = 0, xend = Stop, yend = 0, colour = as.factor(CN))) + facet_wrap(~Chr2, nrow=1, scales = "free") + theme(axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(), legend.position="none") + ylim(0,1) + ylab(IDs[i]) + xlab("")  + scale_colour_manual(values = c("q" = Colors[5],"p" = Colors[4], "1" = Colors[1], "3" = Colors[2], "4" = Colors[3], "0"=Colors[7], "2"=Colors[9]))  #+ ggtitle(IDs[i])
 		p1 <- p1 + geom_vline(aes(xintercept = c(Start), colour=factor(CN)), data=tmp)
