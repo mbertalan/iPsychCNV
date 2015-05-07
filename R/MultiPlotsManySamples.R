5b39167f492757327883065282f62c120e7e072d
Marcelo Bertalan <mbertalan@gmail.com>
2015-05-07 14:52:54 +0200

    Update MultiPlotsManySamples.R

--- R/MultiPlotsManySamples.R
+++ R/MultiPlotsManySamples.R
@@ -21,6 +21,6 @@ MultiPlotsManySamples <- function(df=iPsych_8_Samples_Good, Name="Test.png")
 	   	plots[[i]] <- p1  # add each plot into plot list
 	}
 	png(Name, height=1200, width=1800, res=100)
-	multiplot(plotlist = plots, cols = 1)
+	Multiplot(plotlist = plots, cols = 1)
 	dev.off()
 }
