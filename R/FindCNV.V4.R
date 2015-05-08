07a62806a9ac942d33990b452ed331698b0d08a9
Marcelo Bertalan <mbertalan@gmail.com>
2015-05-08 13:19:21 +0200

    Update FindCNV.V4.R

--- R/FindCNV.V4.R
+++ R/FindCNV.V4.R
@@ -6,7 +6,7 @@
 ##' @author Marcelo Bertalan
 ##' @export
 
-FindCNV.V4 <- function(ID, MinNumSNPs, CNV)
+FindCNV.V4 <- function(ID, MinNumSNPs, CNV, CPTmethod="meanvar", CNVSignal=0.19)
 {
 	
 	suppressPackageStartupMessages(library("changepoint"))
@@ -18,33 +18,20 @@ FindCNV.V4 <- function(ID, MinNumSNPs, CNV)
 		subCNV <- subCNV[with(subCNV, order(Position)),]
 		
 		# Using changepoint package	
-		CPT.Res <- cpt.mean(subCNV$Log.R.Ratio, method='PELT', penalty="AIC", class=TRUE) 
+		if(CPTmethod %in% "meanvar")
+		{
+			CPT.Res <- cpt.meanvar(subCNV$Log.R.Ratio, method='PELT', class=TRUE)
+		}
+		else if(CPTmethod %in% "mean")
+		{
+			CPT.Res <- cpt.mean(subCNV$Log.R.Ratio, method='PELT', penalty="AIC", class=TRUE)
+		}
 		indx <- cpts(CPT.Res)
 		indx <- c(1, indx, length(subCNV$Log.R.Ratio))
 
-
-		# Mergin CNVs #
-		#Info <- sapply(1:(length(indx)-1), function(X){ rbind(indx[X],indx[(X+1)]) })
-		#Info <- t(Info)
-		#Df <- as.data.frame(Info)
-		#CNVMean <- apply(Df, 1, function(X){ mean(CPT.Res@data.set[X[1]:X[2]]) } )
-		#Df$CNVMean <- CNVMean
-		#names(Df) <- c("Start", "Stop", "CNVMean")
-		#tmp <- apply(Df, 1, function(X){ rep(X[3], (as.numeric(X[2])-as.numeric(X[1]))) })
-		#tmp <- unlist(tmp)
-		#tmp[tmp < -0.15] <- -0.5
-		#tmp[tmp > 0.15] <- 0.5
-		#tmp <- as.vector(tmp)
-		#if(length(tmp) > 10)
-		#{
-		#	CPT.Res.tmp <- cpt.mean(tmp, method='PELT', penalty="AIC")
-		#	indx <- cpts(CPT.Res.tmp)
-		#	indx <- c(1, indx, length(tmp))
-		#}
-		###############
-
 		DF <- DefineStartAndStop(indx, subCNV, MinNumSNPs, CHR, ID, CPT.Res)
 		Df <- FixCNVPosition(DF, subCNV, MinNumSNPs, ID) # Fix the position of the CNV by distance of the SNPs
+		Df <- subset(Df, abs(CNVMean) > CNVSignal)
 		return(Df)
 	})
 
