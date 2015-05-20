50c619403d486ce6f796700c81c77154ee3448ff
Marcelo Bertalan <mbertalan@gmail.com>
2015-05-20 13:04:02 +0200

    Create RemoveCentromere.R

---
+++ RemoveCentromere.R
@@ -0,0 +1,26 @@
+##' RemoveCentromere: RemoveCentromere.
+##'
+##' @title RemoveCentromere
+##' @return Data frame with CNV.
+##' @author Marcelo Bertalan
+##' @export
+
+RemoveCentromere <- function(df)
+{
+	tmp <- sapply(unique(df$Chr), function(X)
+	{
+		Cent.tmp <- subset(Cent, Chr %in% X)
+		StartC <- Cent.tmp$Start
+		StopC <- Cent.tmp$Stop
+		
+		# selecting the chr
+		df.tmp <- subset(iPsychCNV_Wave9_V2.good, Chr %in% X)
+		
+		# removing CNVs that overlap centromere
+		df.tmp <- subset(df.tmp, Stop < StartC | Start > StopC)
+		
+		return(df.tmp)
+	})
+	tmp2 <- MatrixOrList2df(tmp)
+	return(tmp2)
+}
