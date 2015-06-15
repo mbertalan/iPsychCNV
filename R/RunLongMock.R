cc7331b5fb784ab15c32ebd2e2886cd1186c27eb
Marcelo Bertalan <mbertalan@gmail.com>
2015-06-15 17:26:37 +0200

    Update RunLongMock.R

--- R/RunLongMock.R
+++ R/RunLongMock.R
@@ -10,22 +10,37 @@ RunLongMock <- function(Name="Test", Method="PennCNV", CNVDistance=1000, Type=c(
 {
 	LongRoi <- MakeLongMockSample(CNVDistance, Type, Mean, Size)
 	Sample <- read.table("LongMockSample.tab", sep="\t", header=TRUE, stringsAsFactors=F)
+	Name <- paste(Name,"_",Method, "_LongMockResult.png", sep="", collapse="")
+	CNVMean=0.3
 	
 	if(Method %in% "PennCNV")
 	{
 		PredictedCNV <- RunPennCNV(PathRawData=".", Pattern="^LongMockSample.tab$", Cores=1, Skip=0, Normalization=FALSE, PFB=NA, HMM=HMM, Path2PennCNV=Path2PennCNV )
+		PlotLRRAndCNVs(PredictedCNV, Sample, CNVMean, Name=Name, Roi=LongRoi)
 	}
 	else if(Method %in% "iPsychCNV")
 	{
 		PredictedCNV <- iPsychCNV(PathRawData=".", MINNumSNPs=28, Cores=1, Pattern="^LongMockSample.tab$", MinLength=10, Skip=0, LCR=FALSE, Quantile=FALSE)
+		PlotLRRAndCNVs(PredictedCNV, Sample, CNVMean, Name=Name, Roi=LongRoi)
 	}
 	else if(Method %in% "Gada")
 	{
 		PredictedCNV <- RunGada(Sample)
+		PlotLRRAndCNVs(PredictedCNV, Sample, CNVMean, Name=Name, Roi=LongRoi)
 	}	
+	else if(Method %in% "All")
+	{
+		PennCNV.Pred <- RunPennCNV(PathRawData=".", Pattern="^LongMockSample.tab$", Cores=1, Skip=0, Normalization=FALSE, PFB=NA, HMM=HMM, Path2PennCNV=Path2PennCNV )
+		PlotLRRAndCNVs(PennCNV.Pred, Sample, CNVMean, Name=Name, Roi=LongRoi)
+		iPsychCNV.Pred <- iPsychCNV(PathRawData=".", MINNumSNPs=28, Cores=1, Pattern="^LongMockSample.tab$", MinLength=10, Skip=0, LCR=FALSE, Quantile=FALSE)
+		PlotLRRAndCNVs(iPsychCNV.Pred, Sample, CNVMean, Name=Name, Roi=LongRoi)
+		Gada.Pred <- RunGada(Sample)
+		PlotLRRAndCNVs(PennCNV.Pred, Sample, CNVMean, Name=Name, Roi=LongRoi)
+		All.Pred <- rbind(PennCNV.Pred, iPsychCNV.Pred, Gada.Pred)
+	}
+		
+		
 
-	Name <- paste(Name,"_",Method, "_LongMockResult.png", sep="", collapse="")
-	CNVMean=0.3
 	PlotLRRAndCNVs(PredictedCNV, Sample, CNVMean, Name=Name, Roi=LongRoi)
 	#CNVs.Eval <­ EvaluateMockResults(LongRoi, PredictedCNV)
 	
