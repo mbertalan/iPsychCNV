ca0a2f21e668845990d8a965c440abf84ac25441
Marcelo Bertalan <mbertalan@gmail.com>
2015-06-23 12:41:58 +0000

    Update ClassNumbers.R

--- R/ClassNumbers.R
+++ R/ClassNumbers.R
@@ -9,13 +9,11 @@
 
 ClassNumbers <- function(tmpRaw)
 {
-	#Centers <- c(0, 0.2, 0.35, 0.5, 0.65, 0.8, 1)
+	#Centers <- c(0, 0.1, 0.3, 0.5, 0.7, 0.9, 1)
 	#names(Centers) <- c("AAAA", "AAAB", "AAB", "AB", "ABB", "ABBB", "BBBB")
-	# Classes
-	# A == 0; B == 0.25, C == 0.5, D == 0.75, E == 1
 	tmp2 <- subset(tmpRaw, PFB > 0 & PFB < 1)
 	UsedBAF <- round((nrow(tmp2)/nrow(tmpRaw))*100, 1)
-	Centers <- c(0, 0.2, 0.35, 0.5, 0.65, 0.8, 1)
+	Centers <- c(0, 0.1, 0.3, 0.5, 0.7, 0.9, 1)
 	names(Centers) <- c("AAAA", "AAAB", "AAB", "AB", "ABB", "ABBB", "BBBB")
 	df <- data.frame(AAAA=0, AAAB=0, AAB=0, AB=0, ABB=0, ABBB=0, BBBB=0)
 	if(nrow(tmp2) > 9)
