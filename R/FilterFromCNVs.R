e247076b04e7499663d0e0719f4189b52448d4a9
Marcelo Bertalan <mbertalan@gmail.com>
2015-06-23 11:13:01 +0000

    Update FilterFromCNVs.R

--- R/FilterFromCNVs.R
+++ R/FilterFromCNVs.R
@@ -12,25 +12,24 @@ FilterFromCNVs <- function(CNVs, PathRawData, MinNumSNPs=10, Source="iPsychCNV",
 
 		Res <- apply(subCNVs, 1, function(X)
 		{
-			#cat(X, "\n")
 			StartM <- as.numeric(X["Start"])
 			StopM <- as.numeric(X["Stop"])
 			ChrM <- X["Chr"]
 			ChrM <- gsub(" ","", ChrM)
 
 			subSample <- subset(Sample, Chr %in% ChrM)
-			#cat(nrow(subSample), "\n")
 			subSample <- subSample[with(subSample, order(Position)),]
-			#NewName <- paste("Test", ChrM, StartM, StopM, ".RData", sep="", collapse="")
-			#save(subSample, Sample, file=NewName)
+			
 			StartIndx <- which(subSample$Position == StartM)[1] 
 			StopIndx <- which(subSample$Position == StopM)[1]
-			Vector <- c(StartIndx, StopIndx, X)
+			df <- data.frame(StartIndx=StartIndx, StopIndx=StopIndx)
+			#Vector <- c(StartIndx, StopIndx, X)
 			cat(ChrM, StartM, StopM, StartIndx, StopIndx, RawFile, "\n")
-			names(Vector)[1:2] <- c("StartIndx", "StopIndx")
-			return(Vector)
+			#names(Vector)[1:2] <- c("StartIndx", "StopIndx")
+			return(df)
 		})
-		tmp <- as.data.frame(t(Res), stringsAsFactors=F)
+		df <- MatrixOrList2df(tmp=Res)
+		tmp <- cbind(subCNVs, df)
 
 		tmp2 <- FilterCNVs.V4(tmp, MinNumSNPs=MinNumSNPs, CNV=Sample, ID=IDs)
 		return(tmp2)
