7ced5f0c98eb28f6107b61fbddddf956cd802842
Marcelo Bertalan <mbertalan@gmail.com>
2015-07-25 23:59:34 +0000

    Update FilterCNVs.V4.R

--- R/FilterCNVs.V4.R
+++ R/FilterCNVs.V4.R
@@ -15,6 +15,7 @@ FilterCNVs.V4 <- function(CNVs = CNVs, MinNumSNPs=10, Sample, ID="Test") #  Path
 	AllRes <- apply(CNVs, 1, function(Y) # Loop for CNVs
 	{  
 		CHR <- Y["Chr"]
+		CHR <- gsub(" ", "", CHR)
 		CNVStart <- as.numeric(Y["Start"]) 
 		CNVStop <- as.numeric(Y["Stop"]) 
 		NumSNPs <- as.numeric(Y["NumSNPs"])
