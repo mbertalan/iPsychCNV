5d6e4a81fa93c40cb26d5201d57f88bdcb613451
Marcelo Bertalan <mbertalan@gmail.com>
2015-07-28 15:09:03 +0000

    Create GetIndxPositionFromChips.R

---
+++ R/GetIndxPositionFromChips.R
@@ -0,0 +1,26 @@
+GetIndxPositionFromChips <- function(CNVs, Sample)
+{
+	Res2 <- sapply(unique(CNVs$Chr), function(y)
+	{
+		df2 <- subset(CNVs, Chr %in% y)	
+		subSample <- subset(Sample, Chr %in% y) 			# Subset by Chr
+		subSample <- subSample[with(subSample, order(Position)),]	
+	
+		Res <- apply(df2, 1, function(X)
+		{
+			StartM <- as.numeric(X["Start"]) # CNV Start
+			StopM <- as.numeric(X["Stop"])
+			# Order
+			StartIndx <- which.min(abs(subSample$Position - StartM))
+			StopIndx <- which.min(abs(subSample$Position - StopM))
+			df <- data.frame(StartIndx=StartIndx, StopIndx=StopIndx)
+			return(df)
+		})
+		df <- MatrixOrList2df(tmp=Res)
+		return(df)
+	})
+	df <- MatrixOrList2df(tmp=Res2)
+	CNVs <- cbind(CNVs, df)
+	CNVs$NumSNPs <- CNVs$StopIndx - CNVs$StartIndx
+	return(CNVs)
+}
