3b70b9301c4f454bc928dfbaf34e462935a2e305
Marcelo Bertalan <mbertalan@gmail.com>
2015-10-30 22:59:14 +0100

    Update GetIndxPositionFromChips.R

--- R/GetIndxPositionFromChips.R
+++ R/GetIndxPositionFromChips.R
@@ -2,8 +2,9 @@ GetIndxPositionFromChips <- function(CNVs, Sample)
 {
 	Res2 <- sapply(unique(CNVs$Chr), function(y)
 	{
-		df2 <- subset(CNVs, Chr %in% y)	
-		subSample <- subset(Sample, Chr %in% y) 			# Subset by Chr
+		CHR <- gsub(" ", "", y)
+		df2 <- subset(CNVs, Chr %in% CHR)	
+		subSample <- subset(Sample, Chr %in% CHR) 			# Subset by Chr
 		subSample <- subSample[with(subSample, order(Position)),]	
 	
 		Res <- apply(df2, 1, function(X)
