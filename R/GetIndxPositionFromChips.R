84d37f4363f5f4f8e73790dffbdf2ff944746428
Marcelo Bertalan <mbertalan@gmail.com>
2015-10-31 11:19:41 +0100

    Update GetIndxPositionFromChips.R

--- R/GetIndxPositionFromChips.R
+++ R/GetIndxPositionFromChips.R
@@ -1,26 +1,28 @@
 GetIndxPositionFromChips <- function(CNVs, Sample)
 {
-	Res <- apply(CNVs, 1, function(X)
+	Res2 <- sapply(unique(CNVs$Chr), function(y)
 	{
-		#cat(X, "\n")
-		StartM <- as.numeric(X["Start"]) # CNV Start
-		StopM <- as.numeric(X["Stop"])
-		CHR <- X["Chr"]
-		#cat("Sample rows:", nrow(Sample), "\n")
-		CHR <- gsub(" ", "", CHR)	
+		CHR <- y
+		CHR <- gsub(" ", "", CHR)
+		df2 <- subset(CNVs, Chr %in% CHR)	
 		subSample <- subset(Sample, Chr %in% CHR) 			# Subset by Chr
 		subSample <- subSample[with(subSample, order(Position)),]	
-		#cat("SubSample rows:", nrow(subSample), "\n")
-		# Order
-		StartIndx <- which.min(abs(subSample$Position - StartM))
-		StopIndx <- which.min(abs(subSample$Position - StopM))
-		NumSNPs <- StopIndx - StartIndx
-		#cat(StartIndx, StopIndx, "\n")
-		df <- data.frame(StartIndx=StartIndx, StopIndx=StopIndx, NumSNPs=NumSNPs)
+	
+		Res <- apply(df2, 1, function(X)
+		{
+			StartM <- as.numeric(X["Start"]) # CNV Start
+			StopM <- as.numeric(X["Stop"])
+			# Order
+			StartIndx <- which.min(abs(subSample$Position - StartM))
+			StopIndx <- which.min(abs(subSample$Position - StopM))
+			NumSNPs <- StopIndx - StartIndx
+			df <- data.frame(StartIndx=StartIndx, StopIndx=StopIndx, NumSNPs=NumSNPs)
+			df2 <- cbind(X, df)
+			return(df2)
+		})
+		df <- MatrixOrList2df(tmp=Res)
 		return(df)
 	})
-	#save(Res, file="Res.RData")
-	df <- MatrixOrList2df(Res)
-	df2 <- cbind(CNVs, df)	
-	return(df2)
+	df <- MatrixOrList2df(tmp=Res2)
+	return(CNVs)
 }
