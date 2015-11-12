065bbe3597cf1cd0108a3055e2ee545736754f0c
Marcelo Bertalan <mbertalan@gmail.com>
2015-11-12 14:39:29 +0100

    Update FindHighFreqCNVs.R

--- R/FindHighFreqCNVs.R
+++ R/FindHighFreqCNVs.R
@@ -1,3 +1,11 @@
+##' iPsychCNV: Find Copy Number Variation (CNV) from SNP genotyping arrays. 
+##'
+##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' @title iPsychCNV
+##' @return Data frame with CNVs predicted.
+##' @author Marcelo Bertalan
+##' @export
+
 FindHighFreqCNVs <- function(df, OverlapCutoff=0.8)
 {
 	#tmp <- tapply(df$Start, as.factor(df$Chr), function(X){ sort(table(X), decreasing=TRUE) })
