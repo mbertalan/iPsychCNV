f12c9efe4d9a59fe48c127e2f232f04acebf81e7
Marcelo Bertalan <mbertalan@gmail.com>
2015-11-12 14:44:48 +0100

    Update MergeCNVs.R

--- R/MergeCNVs.R
+++ R/MergeCNVs.R
@@ -1,3 +1,11 @@
+##' iPsychCNV: Find Copy Number Variation (CNV) from SNP genotyping arrays. 
+##'
+##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' @title iPsychCNV
+##' @return Data frame with CNVs predicted.
+##' @author Marcelo Bertalan
+##' @export
+
 MergeCNVs <- function(df, MaxNumSNPs=50)
 {
 	tmp2 <- subset(df, CN != 2)
