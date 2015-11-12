0fd52c0bdea65687d14cd8a4dd638570459c54db
Marcelo Bertalan <mbertalan@gmail.com>
2015-11-12 14:47:28 +0100

    Update RemoveCentromere.R

--- R/RemoveCentromere.R
+++ R/RemoveCentromere.R
@@ -1,4 +1,12 @@
-RemoveCentromere <- function(df, HG="hg18")
+##' iPsychCNV: Find Copy Number Variation (CNV) from SNP genotyping arrays. 
+##'
+##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' @title iPsychCNV
+##' @return Data frame with CNVs predicted.
+##' @author Marcelo Bertalan
+##' @export
+
+RemoveCentromere <- function(df, HG="hg19")
 {
 	Cent <- subset(Cent, hg %in% HG)
 	tmp <- sapply(unique(df$Chr), function(X)
