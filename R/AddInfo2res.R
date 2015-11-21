b7f90994e24d0f536637e00f0faee06822ecc4b6
Marcelo Bertalan <mbertalan@gmail.com>
2015-11-21 13:29:46 +0100

    Update AddInfo2res.R

--- R/AddInfo2res.R
+++ R/AddInfo2res.R
@@ -1,8 +1,27 @@
-##' AddInfo2res: read data. 
+##' iPsychCNV: Find Copy Number Variation (CNV) from SNP genotyping arrays. 
+##' Specifically designed to reduce false positive CNVs and amplified DNA on dried blood spots.
+
+##' AddInfo2res: Function to add variables for each CNV region. Function is used at FilterCNVs.V4.R.
 ##'
-##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
 ##' @title AddInfo2res
-##' @return return data in data frame
+
+##' @param res: cbind of two data frames. 1-) GetDataVariables. 2-) ClassNumbers.
+##' @param CNV2HighPvalue: p value of t.test among CNV region and its upstream region.
+##' @param CNV2LowPvalue: p value of t.test among CNV region and its downstream region.
+##' @param Class: Result from DefineBAFType function.
+##' @param BAlleleFreq: Result from DefineBAFType function.
+##' @param MyBAF: Result from EvaluateMyBAF function.
+##' @param LogRRatio: Result from  DefiningLogRRatio function.
+##' @param SumPeaks: Result from CleaningPeaks function.
+##' @param SDChr: LRR standard deviation from whole chromosome. 
+##' @param MeanChr: LRR mean from whole chromosome.
+##' @return return all variables together in a in data frame
+
+##' @author Marcelo Bertalan
+##' @export
+##' @examples
+##' res4 <- AddInfo2res(res3, CNV2HighPvalue, CNV2LowPvalue, Class, BAlleleFreq, MyBAF, LogRRatio, SumPeaks, SDChr, MeanChr)
+
 ##' @author Marcelo Bertalan
 ##' @export
 
