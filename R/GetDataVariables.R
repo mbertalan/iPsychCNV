b91e23a5bcdfdf7b4247c677dd41a6f9cdf6d742
Marcelo Bertalan <mbertalan@gmail.com>
2015-12-08 14:20:52 +0100

    Update GetDataVariables.R

--- R/GetDataVariables.R
+++ R/GetDataVariables.R
@@ -31,28 +31,29 @@ GetDataVariables <- function(Data)
 	DiffLow <- abs(CNVmean - LowMean)
 	
 	# Wave
-	tp <- turnpoints(Data$LRR)
-	Peaks <- c(1,which(tp$peaks))
-	Pits <- c(1,which(tp$pits))
+	#tp <- turnpoints(Data$LRR)
+	#Peaks <- c(1,which(tp$peaks))
+	#Pits <- c(1,which(tp$pits))
 
-	WaveLenPeaks <- sapply(2:length(Peaks), function(X){ Peaks[X] - Peaks[X-1] })
-	WaveLenPits <- sapply(2:length(Pits), function(X){ Pits[X] - Pits[X-1] })
+	#WaveLenPeaks <- sapply(2:length(Peaks), function(X){ Peaks[X] - Peaks[X-1] })
+	#WaveLenPits <- sapply(2:length(Pits), function(X){ Pits[X] - Pits[X-1] })
 		
-	WaveLength <- rep(0, length(tp$peaks))
-	WaveLength[tp$peaks] <- WaveLenPeaks
-	WaveLength[tp$pits] <- WaveLenPits
-	Wave <- WaveLength[WaveLength > 0]
+	#WaveLength <- rep(0, length(tp$peaks))
+	#WaveLength[tp$peaks] <- WaveLenPeaks
+	#WaveLength[tp$pits] <- WaveLenPits
+	#Wave <- WaveLength[WaveLength > 0]
 
-	Indx <- sort(c(Peaks,Pits))[-1]
-	Points <- tp$points[Indx]
-	Points2 <- Points + abs(min(Points))
-	Amplitude <- sapply(1:(length(Points2)-1), function(X){ Points2[X] - Points2[X+1] })
-	Wave.Length.Mean <- mean(Wave)
-	Wave.Length.SD <- sd(Wave)
-	Amplitude.Mean <- mean(Amplitude)
-	Amplitude.SD <- sd(Amplitude)
+	#Indx <- sort(c(Peaks,Pits))[-1]
+	#Points <- tp$points[Indx]
+	#Points2 <- Points + abs(min(Points))
+	#Amplitude <- sapply(1:(length(Points2)-1), function(X){ Points2[X] - Points2[X+1] })
+	#Wave.Length.Mean <- mean(Wave)
+	#Wave.Length.SD <- sd(Wave)
+	#Amplitude.Mean <- mean(Amplitude)
+	#Amplitude.SD <- sd(Amplitude)
 
-	res2 <- data.frame(CNVmean=CNVmean, HighMean=HighMean, LowMean=LowMean, CNVmeanOrig=CNVmeanOrig, SDCNV=SDCNV, SDMeansCNV=SDMeansCNV, SDHigh=SDHigh, SDLow=SDLow,VarianceCNV=VarianceCNV, DiffHigh=DiffHigh, DiffLow=DiffLow,  Wave.Length.Mean=Wave.Length.Mean, Wave.Length.SD=Wave.Length.SD, Amplitude.Mean=Amplitude.Mean, Amplitude.SD=Amplitude.SD)	
+	#res2 <- data.frame(CNVmean=CNVmean, HighMean=HighMean, LowMean=LowMean, CNVmeanOrig=CNVmeanOrig, SDCNV=SDCNV, SDMeansCNV=SDMeansCNV, SDHigh=SDHigh, SDLow=SDLow,VarianceCNV=VarianceCNV, DiffHigh=DiffHigh, DiffLow=DiffLow,  Wave.Length.Mean=Wave.Length.Mean, Wave.Length.SD=Wave.Length.SD, Amplitude.Mean=Amplitude.Mean, Amplitude.SD=Amplitude.SD)
+	res2 <- data.frame(CNVmean=CNVmean, HighMean=HighMean, LowMean=LowMean, CNVmeanOrig=CNVmeanOrig, SDCNV=SDCNV, SDMeansCNV=SDMeansCNV, SDHigh=SDHigh, SDLow=SDLow,VarianceCNV=VarianceCNV, DiffHigh=DiffHigh, DiffLow=DiffLow)
 	return(res2)	
 	
 }
