2c7ca36ade8679f17e6e4867faa0f4fe8b8fb4e6
Marcelo Bertalan <mbertalan@gmail.com>
2016-01-16 11:23:06 -0200

    Update PlotLRRAndCNVs.R

--- R/PlotLRRAndCNVs.R
+++ R/PlotLRRAndCNVs.R
@@ -18,6 +18,20 @@ PlotLRRAndCNVs <- function(CNV, Sample=MockData, CNVMean=0.3, Name="Test.png", R
 		CNV$Source <- "iPsychCNV" 
 	}
 	
+	if(length(CNV$CNVmean) == 0)
+	{
+		if(length(CNV$CNVMean) == 0)
+		{
+			CNV$CNVmean <- rep(CNVMean, nrow(CNV))
+			CNV$CNVmean[CNV$CN == 1] <- CNV$CNVmean[CNV$CN == 1] * -1
+			CNV$CNVmean[CNV$CN == 0] <- Roi$CNVmean[CNV$CN == 0] * -1
+		}
+		else
+		{
+			CNV$CNVmean <- CNV$CNVMean
+		}
+	}
+	
 	if(length(Roi$CNVmean) == 0)
 	{
 		Roi$CNVmean <- rep(CNVMean, nrow(Roi))
@@ -35,15 +49,13 @@ PlotLRRAndCNVs <- function(CNV, Sample=MockData, CNVMean=0.3, Name="Test.png", R
 	Colors2 = brewer.pal("Dark2", n=7)
 
 	# LRR
-	p1 <- ggplot(tmp, aes(Position, Log.R.Ratio))
-	p1 <- p1 + geom_point(alpha=0.05, size=1) 
+	p1 <- ggplot(tmp, aes(x=Position, y=Log.R.Ratio)) + geom_point(aes(x=Position, y=Log.R.Ratio), alpha=0.05, size=1) 
 	p1 <- p1 + geom_rect(data=Roi, aes(xmin =Start, xmax=Stop, ymin=(CNVmean-0.1), ymax=(CNVmean+0.1)), colour=Colors2[3], fill=NA, alpha=0.3, inherit.aes = FALSE, size=0.5) + theme(legend.title=element_blank())
 	p1 <- p1 +  geom_segment(data=CNV, aes(x = Start, y = CNVmean, xend = Stop, yend = CNVmean, colour=as.factor(CN)), size=4) + scale_colour_manual(values = c("1" = Colors[1], "2"=Colors[9], "3" = Colors[2], "4" = Colors[3], "0"=Colors[4], "B.Allele.Freq"=Colors[2], "CNV region"=Colors[3], "CNV predicted"=Colors[4], "Mean"="black"))	
 	p1 <- p1 + geom_line(data=tmp, aes(x=Position, y = Mean), size = 0.1, alpha=0.2) 
 	
 	# BAF
-	p2 <- ggplot(tmp, aes(Position, y = B.Allele.Freq)) 
-	p2 <- p2 + geom_point(aes(col="B.Allele.Freq"), alpha=0.3, size=1)  
+	p2 <- ggplot(tmp, aes(x=Position, y=B.Allele.Freq)) + geom_point(aes(x=Position, y = B.Allele.Freq, col="B.Allele.Freq"), alpha=0.3, size=1) 
 	p2 <- p2 + geom_rect(data=Roi, aes(xmin=Start, xmax=Stop, ymin=YMin, ymax=YMax, col="CNV region"), alpha=0.2, inherit.aes = FALSE) + theme(legend.title=element_blank()) 
 	
 	CNV <- subset(CNV, CN != 2)
