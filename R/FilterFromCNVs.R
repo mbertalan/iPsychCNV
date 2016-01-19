ba8388e504a09e41ebab8ab4614893bd62d4ab02
Marcelo Bertalan <mbertalan@gmail.com>
2016-01-19 14:29:21 -0200

    Update FilterFromCNVs.R

--- R/FilterFromCNVs.R
+++ R/FilterFromCNVs.R
@@ -63,12 +63,12 @@ FilterFromCNVs <- function(CNVs, PathRawData, MinNumSNPs=10, Source="iPsychCNV",
 	tmp2 <- MatrixOrList2df(tmp)
 	df <- tmp2[, !colnames(tmp2) %in% ".id"]
 	df$Source <- Source
-	df$CN <- df$Class
-	df$CN[df$CN %in% "Del"] <- "1"
-	df$CN[df$CN %in% "Normal"] <- "2"
-	df$CN[df$CN %in% "Dup"] <- "3"
-	df$CN[df$CN %in% "DoubleDel"] <- "0"
-	df$CN[df$CN %in% "DoubleDup"] <- "4"
-	df$CN <- as.numeric(df$CN)
+	#df$CN <- df$Class
+	#df$CN[df$CN %in% "Del"] <- "1"
+	#df$CN[df$CN %in% "Normal"] <- "2"
+	#df$CN[df$CN %in% "Dup"] <- "3"
+	#df$CN[df$CN %in% "DoubleDel"] <- "0"
+	#df$CN[df$CN %in% "DoubleDup"] <- "4"
+	#df$CN <- as.numeric(df$CN)
 	return(df)
 }
