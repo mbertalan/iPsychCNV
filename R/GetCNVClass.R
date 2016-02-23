f8f1b2667dd374b013242e10df285a2e0b491eff
lhoeffding <louise.k.enggaard.hoeffding@regionh.dk>
2016-02-23 21:14:50 +0100

    Update GetCNVClass.R

--- R/GetCNVClass.R
+++ R/GetCNVClass.R
@@ -1,10 +1,13 @@
-##' GetCNVClass: read data. 
+##' GetCNVClass: Read data. 
 ##'
-##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' Specifically designed to handle noisy data from amplified DNA on phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
 ##' @title GetCNVClass
-##' @return return data in data frame
-##' @author Marcelo Bertalan
+##' @param Variable: Numeric, Unknown.
+##' @return return data in data frame.
+##' @author Marcelo Bertalan, Louise K. Hoeffding.
+
 ##' @export
+##' @examples Unknown.
 
 GetCNVClass <- function(Variable) # Can be 0, 1, 3, 4
 {
