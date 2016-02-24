aac3718cb8b9ac1c04cd4c6a0ccd8dcc30ef2219
lhoeffding <louise.k.enggaard.hoeffding@regionh.dk>
2016-02-24 10:46:56 +0100

    Update RunSingleMock.R

--- R/RunSingleMock.R
+++ R/RunSingleMock.R
@@ -1,10 +1,17 @@
-##' RunSingleMock: Run a single mock sample   
+##' RunSingleMock: Run a single mock sample.   
 ##'
-##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' Specifically designed to handle noisy data from amplified DNA on phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
 ##' @title RunSingleMock
-##' @return Classification for LRR.
-##' @author Marcelo Bertalan
+##' @param Noise: Unknown, default = 0.
+##' @param CNVMean: Unknown, default = 0.4.
+##' @param Name: Unknown, default =Unknown.
+##' @param Method: The CNV predicting algorithm, default = PennCNV.
+##' @return Classification for Log R Ratio (LRR).
+##' @author Marcelo Bertalan, Louise K. Hoeffding.
+##' @source \url{http://biopsych.dk/iPsychCNV}
 ##' @export
+##' @examples Unknown
+##'
 
 RunSingleMock <- function(Noise=0, CNVMean=0.4, Name="Test", Method="PennCNV")
 {
