7863972d4c4702cacd8bfae85f1dde46e713e288
lhoeffding <louise.k.enggaard.hoeffding@regionh.dk>
2016-02-24 09:49:35 +0100

    Update QC_plots.R

--- R/QC_plots.R
+++ R/QC_plots.R
@@ -1,10 +1,14 @@
-##' iPsychCNV: Find Copy Number Variation (CNV) from SNP genotyping arrays. 
+##' QC_plots: QC plots. 
 ##'
-##' Specifically designed to handle noisy data from amplified DNA on  Phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
-##' @title iPsychCNV
-##' @return Data frame with CNVs predicted.
-##' @author Marcelo Bertalan
+##' Specifically designed to handle noisy data from amplified DNA on phenylketonuria (PKU) cards. The function is a pipeline using many subfunctions.
+##' @title QC_plots
+##' @param tmp: Unknown, default = Unknown. 
+##' @return Unknown.
+##' @author Marcelo Bertalan, Louise K. Hoeffding.
+##' @source \url{http://biopsych.dk/iPsychCNV}
 ##' @export
+##' @examples Unknown
+##'
 
 QC_Plots <- function(tmp)
 {
