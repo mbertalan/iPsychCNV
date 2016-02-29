b2ba466d5b0978b828fa0b9eb63a044f1b52a66e
Stefan Möding <stm@kill-9.net>
2016-02-29 20:47:45 +0100

    Update documentation for format_si()

--- R/format_si.R
+++ R/format_si.R
@@ -1,20 +1,19 @@
 ##' format_si: Format a vector of numeric values according to the International System of Units.
 ##'
 ##' Format a vector of numeric values according to the International System of Units.
-##' http://en.wikipedia.org/wiki/SI_prefix
-##' Based on code by Ben Tupper
-##' https://stat.ethz.ch/pipermail/r-help/2012-January/299804.html
-##' Args:
-##'   ...: Args passed to format()
+##' \code{\link{http://en.wikipedia.org/wiki/SI_prefix}}
+##'
+##' Based on code by Ben Tupper (\code{\link{https://stat.ethz.ch/pipermail/r-help/2012-January/299804.html}})
 ##'
 ##' @title format_si
-##' @param x: A vector of numeric values
+##' @param \dots additional arguments to format()
 ##' @return A function to format a vector of strings using SI prefix notation
-##' @author Stefan Möding (http://www.moeding.net/archives/32-Metric-prefixes-for-ggplot2-scales.html)
-##' @source \url{http://biopsych.dk/iPsychCNV}
+##' @author Stefan Möding
+##' @source \url{http://www.moeding.net/archives/32-Metric-prefixes-for-ggplot2-scales.html}
 ##' @export
-##' @examples Unknown
-
+##' @examples
+##' ggplot() + ... + scale_y_continuous(labels=format_si())
+##'
 format_si <- function(...) {
   function(x) {
     limits <- c(1e-24, 1e-21, 1e-18, 1e-15, 1e-12,
