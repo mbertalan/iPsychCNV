2a3038639d68fd57eccff8979ad3dbc78130ef5c
Ida Elken Sønderby <i.e.sonderby@medisin.uio.no>
2016-04-12 14:58:05 +0200

    Updated RescanCNVs.R 	-with SNPList (after merging with Marcello's changes)

--- R/ReScanCNVs.R
+++ R/ReScanCNVs.R
@@ -44,77 +44,76 @@
 
 ReScanCNVs <- function(CNVs=CNVs, PathRawData = "/media/NeoScreen/NeSc_home/ILMN/iPSYCH/", MINNumSNPs=5, Cores=1, hg="hg19", NumFiles="All", Pattern="*", MinLength=10, SelectedFiles=NA, Skip=10, LCR=FALSE, PFB=NULL, chr=NA, penalty=60, Quantile=FALSE, QSpline=FALSE, sd=0.18, recursive=FALSE, CPTmethod="meanvar", CNVSignal=0.1, penvalue=10, OutputPath=NA, IndxPos=FALSE, ResPerSample=FALSE, Files=NA, OnlyCNVs=TRUE, SNPList=NULL) # Files2 OutputPath
 {
-	if(file.exists("Progress.txt")){ file.remove("Progress.txt") }
-	suppressPackageStartupMessages(library(parallel))
-	ptm <- proc.time()
+  if(file.exists("Progress.txt")){ file.remove("Progress.txt") }
+  suppressPackageStartupMessages(library(parallel))
+  ptm <- proc.time()
 
-	if(is.na(Files))
-	{
-		Files <- list.files(path=PathRawData, pattern=Pattern, full.names=TRUE, recursive=recursive)
-	}
+  if(is.na(Files))
+  {
+    Files <- list.files(path=PathRawData, pattern=Pattern, full.names=TRUE, recursive=recursive)
+  }
 
-	if(length(SelectedFiles) > 1 & !is.na(SelectedFiles[1]))
-	{
-		Files <- sapply(SelectedFiles, function(X){ Files[grep(X, Files)] })
-	}
+  if(length(SelectedFiles) > 1 & !is.na(SelectedFiles[1]))
+  {
+    Files <- sapply(SelectedFiles, function(X){ Files[grep(X, Files)] })
+  }
 
-	if(NumFiles %in% "All"){ NumFiles <- length(Files) }
+  if(NumFiles %in% "All"){ NumFiles <- length(Files) }
 
-	cat("Running ", NumFiles, "files\n")
-	tmp <- mclapply(Files[1:NumFiles], mc.cores=Cores, mc.preschedule = FALSE, function(X)
-	{
-	  RawFile <- X
-	  write(X,file="Progress.txt",append=TRUE)
-	  Count <- length(readLines("Progress.txt"))
-	  Percent <- round((Count/NumFiles)*100)
-	  Percent <- paste(Percent, "%", sep="", collapse="")
-	  cat("Running:\t", X, "\t\t", Percent, "\n")
-	  ID <- tail(unlist(strsplit(X, "/")),n=1)
+  cat("Running ", NumFiles, "files\n")
+  tmp <- mclapply(Files[1:NumFiles], mc.cores=Cores, mc.preschedule = FALSE, function(RawFile)
+  {
+    write(RawFile,file="Progress.txt",append=TRUE)
+    Count <- length(readLines("Progress.txt"))
+    Percent <- round((Count/NumFiles)*100)
+    Percent <- paste(Percent, "%", sep="", collapse="")
+    cat("Running:\t", RawFile, "\t\t", Percent, "\n")
+    ID <- tail(unlist(strsplit(RawFile, "/")),n=1)
 
-	  # Read sample file
-	  ptm.tmp <- proc.time()
-	  Sample <- ReadSample(RawFile, skip=Skip, LCR=LCR, PFB=PFB, chr=chr, SNPList=SNPList)
+    # Read sample file
+    ptm.tmp <- proc.time()
+    Sample <- ReadSample(RawFile, skip=Skip, LCR=LCR, PFB=PFB, chr=chr, SNPList=SNPList)
 
-		# The CNVs are given by hotspots, Position might change if multiple chips are used.
-		if(IndxPos)
-		{
-			CNVs <- GetIndxPositionFromChips(CNVs, Sample)
-		}
+    # The CNVs are given by hotspots, Position might change if multiple chips are used.
+    if(IndxPos)
+    {
+      CNVs <- GetIndxPositionFromChips(CNVs, Sample)
+    }
 
-		if(nrow(CNVs) > 0)
-		{
-			CNVs <- subset(CNVs, NumSNPs > MINNumSNPs)
-			df <- FilterCNVs.V4(CNVs = CNVs, MinNumSNPs=MINNumSNPs, Sample=Sample, ID) # PathRawData = PathRawData,
+    if(nrow(CNVs) > 0)
+    {
+      CNVs <- subset(CNVs, NumSNPs > MINNumSNPs)
+      df <- FilterCNVs.V4(CNVs = CNVs, MinNumSNPs=MINNumSNPs, Sample=Sample, ID) # PathRawData = PathRawData,
 
-			# removing non-CNV results to save memory
-			if(OnlyCNVs)
-			{
-				df <- subset(df, CN != 2)
-			}
+      # removing non-CNV results to save memory
+      if(OnlyCNVs)
+      {
+        df <- subset(df, CN != 2)
+      }
 
-			# Save memory if too many CNVRs to ReScan. Print results or return as an object ?
-			if(ResPerSample)
-			{
-				OutputFile <- paste(OutputPath, ID, ".CNVs", sep="", collapse="")
-				write.table(df, file=OutputFile, sep="\t", quote=FALSE, row.names=FALSE)
-			}
-			else
-			{
-				return(df)
-			}
-		}
-		Res.tmp <- proc.time() - ptm.tmp
-	})
-	cat("Done all !\n")
-	df <- MatrixOrList2df(tmp)
+      # Save memory if too many CNVRs to ReScan. Print results or return as an object ?
+      if(ResPerSample)
+      {
+        OutputFile <- paste(OutputPath, ID, ".CNVs", sep="", collapse="")
+        write.table(df, file=OutputFile, sep="\t", quote=FALSE, row.names=FALSE)
+      }
+      else
+      {
+        return(df)
+      }
+    }
+    Res.tmp <- proc.time() - ptm.tmp
+  })
+  cat("Done all !\n")
+  df <- MatrixOrList2df(tmp)
 
-	if(!is.na(OutputPath))
-	{
-		OutputFile <- paste(OutputPath, "All_CNVs.tab", sep="", collapse="")
-		write.table(df, file=OutputFile, sep="\t", quote=FALSE, row.names=FALSE)
-	}
+  if(!is.na(OutputPath))
+  {
+    OutputFile <- paste(OutputPath, "All_CNVs.tab", sep="", collapse="")
+    write.table(df, file=OutputFile, sep="\t", quote=FALSE, row.names=FALSE)
+  }
 
-	TimeRes <- proc.time() - ptm
-	cat("Total time: ", TimeRes["elapsed"], "\n")
-	return(df)
+  TimeRes <- proc.time() - ptm
+  cat("Total time: ", TimeRes["elapsed"], "\n")
+  return(df)
 }
