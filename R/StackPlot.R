f10b68013fed68da7e195216877af78307fa40d6
Marcelo Bertalan <mbertalan@gmail.com>
2016-08-12 16:15:44 +0200

    Update StackPlot.R

--- R/StackPlot.R
+++ R/StackPlot.R
@@ -36,10 +36,6 @@ StackPlot <- function(Pos, Ids, PathRawData, CNVs, Highlight = NULL, SNPList=NUL
   if(length(Ids) < 1) {
     stop("Please specify minimum ID\n")
   }
-  
-  if(sum(is.na(Ids) > 1) {
-    stop("Your Ids have NAs, please insert only valid IDs\n")
-  }
 
   ## Check highlight position if specified
   if (length(Highlight) > 0){
