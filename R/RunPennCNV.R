b748333be7a07b92cda07d741110680df37f3ad4
Marcelo Bertalan <mbertalan@gmail.com>
2016-10-03 12:27:32 +0200

    Update RunPennCNV.R

--- R/RunPennCNV.R
+++ R/RunPennCNV.R
@@ -17,6 +17,7 @@
 ##' @param Sd: numeric, LRR standard deviation (sd) for the quantile nomarlization, default = 0.15.
 ##' @param PennCNVFormat: Unknown, default = FALSE.
 ##' @param RemoveTmpfiles: Unknown, default = TRUE.
+##' @param Files: a vector with all samples name and path. If too many samples, list all files using recursive=TRUE can take long time. 
 ##' @return Data frame with predicted CNVs.
 ##' @author Marcelo Bertalan, Louise K. Hoeffding.
 ##' @source \url{http://biopsych.dk/iPsychCNV}
@@ -24,11 +25,20 @@
 ##' @examples Unknown.
 ##'
 
-RunPennCNV <- function(PathRawData = "~/CNVs/MockData/PKU/Data", MINNumSNPs=20, Pattern=".*Mock.*\\.tab$", Cores=20, Skip=0, Normalization=FALSE, PFB="NO", HMM="/media/NeoScreen/NeSc_home/share/Programs/penncnv/lib/hhall.hmm", Path2PennCNV="/media/NeoScreen/NeSc_home/share/Programs/penncnv/",  penalty=60, Quantile=TRUE, QSpline=TRUE, sd=0.15, PennCNVFormat=FALSE, RemoveTmpfiles=TRUE)
+RunPennCNV <- function(PathRawData = "~/CNVs/MockData/PKU/Data", MINNumSNPs=20, Pattern=".*Mock.*\\.tab$", Cores=20, Skip=0, Normalization=FALSE, PFB="NO", HMM="/media/NeoScreen/NeSc_home/share/Programs/penncnv/lib/hhall.hmm", Path2PennCNV="/media/NeoScreen/NeSc_home/share/Programs/penncnv/",  penalty=60, Quantile=TRUE, QSpline=TRUE, sd=0.15, PennCNVFormat=FALSE, Files=NA, RemoveTmpfiles=TRUE)
 {
 	library(parallel)
-	Files <- list.files(path=PathRawData, pattern=Pattern, full.names=TRUE, recursive=FALSE)
-
+	
+	suppressWarnings(if(is.na(Files))
+	{
+		Files <- list.files(path=PathRawData, pattern=Pattern, full.names=TRUE, recursive=recursive)
+	})
+	
+	if(length(SelectedFiles) > 1 & !is.na(SelectedFiles[1]))
+	{
+		Files <- sapply(SelectedFiles, function(X){ Files[grep(X, Files)] })
+	}
+	
 	# Rewriting file.
 	cat("Rewriting files to fit PennCNV format.\n")
 	if(!PennCNVFormat) # If not in PennCNV format, than make files to match PennCNV format.
