4552836b6d3ec6582f28cd15ce403a6fc9baa166
Marcelo Bertalan <mbertalan@gmail.com>
2017-04-20 10:35:48 +0200

    Update EvaluateMyBAF.R

--- R/EvaluateMyBAF.R
+++ R/EvaluateMyBAF.R
@@ -33,14 +33,14 @@ EvaluateMyBAF <- function(res, res2)
 	{
 		#Centers <- c(0, 0.25, 0.34, 0.5, 0.67, 0.75, 1)
 		#names(Centers) <- c("AAAA", "AAAB", "AAB", "AB", "ABB", "ABBB", "BBBB")
-		if(sum(sum(res$AAB > 5), sum(res$AB > 5), sum(res$ABB > 5), sum(res$AAAB > 5), sum(res$ABBB > 5)) == 3 & res$AB > 5) # Double Dup AB = AABB, res$AAAB < 6 & res$ABBB < 6 & res$AB > 5 & res$AAAA > 7 & res$BBBB > 7 & res$AAB > 6 & res$ABB > 6
+		if(res$AAB > res$AB & res$ABB > res$AB) # Dup 3, 10, 3
+		{
+			BAlleleFreq <- 3
+		}
+		else if(sum(sum(res$AAB > 5), sum(res$AB > 5), sum(res$ABB > 5), sum(res$AAAB > 5), sum(res$ABBB > 5)) == 3 & res$AB > 5) # Double Dup AB = AABB, res$AAAB < 6 & res$ABBB < 6 & res$AB > 5 & res$AAAA > 7 & res$BBBB > 7 & res$AAB > 6 & res$ABB > 6
 		{
 			# At least one of the correct positions need to be more than 5%
 			if(res$AAAB > 5 || res$ABBB > 5){ BAlleleFreq <- 4}
-		}
-		else if(res$AAB > res$AB & res$ABB > res$AB) # Dup 3, 10, 3
-		{
-			BAlleleFreq <- 3
 		}
 		else if(sum(sum(res$AAB > 5), sum(res$AB > 5), sum(res$ABB > 5), sum(res$AAAB > 5), sum(res$ABBB > 5)) == 2 & res$AB < 5) # Dup 3, 10, 3			BAlleleFreq <- 2
 		{
